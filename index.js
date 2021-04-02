5d1f62ff2179d51c807b7cb1d27a6a881106f671
unknown <chebirr@yahoo.com>
2021-04-02 19:03:57 -0400

    my first commit

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ console.log ('Hello world')
 let firstname = 'kabiru'
 let greeting = "hello" + firstname
 console.log('greeting')
+let midname = 'mustapha'
+let salute =`welcome ${midname}`
+console.log('let the welcome message be', salute)
