db0d8bb0cd5ae9407c0865fe9696fcb8a6342f80
unknown <chebirr@yahoo.com>
2021-04-02 20:16:58 -0400

    my first commit

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ let greeting = "hello" + firstname
 console.log('greeting')
 let midname = 'mustapha'
 let salute =`welcome ${midname}`
+let address = '350 mushin road'
+console.log('my old address is', address)
 console.log('let the welcome message be', salute)
